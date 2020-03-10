58de52ebdf16e71b208431adc8adc8c0cc448c98
iFission <alexwwj@icloud.com>
2020-03-11 00:43:08 +0800

    feat: add dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./Components/styles.scss";
 import Content from "./Components/Main.js";
+import DarkModeToggle from "./Components/DarkModeToggle";
 
 function App() {
-  return <Content />;
+  return (
+    <>
+      <div className="navbar">
+        <Content />
+        <DarkModeToggle />
+      </div>
+    </>
+  );
 }
 
 export default App;
