e0fbb148b3542770892938367a81b3c1b6eefddd
iFission <alexwwj@icloud.com>
2020-07-01 13:15:39 +0800

    deploy react

---
+++ precache-manifest.7e152502bf4fa9078761ba1ed0f0d80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd024ef5c96ee422a485a0ecd7e02918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e29ea2ad3c4502c6c75",
+    "url": "/static/css/main.37f92fa1.chunk.css"
+  },
+  {
+    "revision": "4af3a1211b10957f1424",
+    "url": "/static/js/2.c7eec0f0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c7eec0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e29ea2ad3c4502c6c75",
+    "url": "/static/js/main.d18c4a66.chunk.js"
+  },
+  {
+    "revision": "5a0af4100d1e79bd9287",
+    "url": "/static/js/runtime-main.e5894968.js"
+  }
+]);
